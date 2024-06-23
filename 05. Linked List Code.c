b4933cac20c73919abbd9757e67c8265df104ca4
Md Jakaria Nur <122019360+mdjakarianurs@users.noreply.github.com>
2024-06-23 09:40:08 +0600

    Create 05. Linked List Code.c

---
+++ 05. Linked List Code.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  struct node
+  {
+      int data;
+      struct node *next;
+  };
+  struct node *head,*middle,*last;
+  head   = malloc(sizeof(struct node));
+  middle = malloc(sizeof(struct node));
+  last   = malloc(sizeof(struct node));
+  head->data   = 10;
+  middle->data = 20;
+  last->data   = 30;
+  head->next   = middle;
+  middle->next = last;
+  last->next   = NULL;
+  struct node *temp = head;
+  while(temp != NULL)
+  {
+      printf("%d->",temp->data);
+      temp = temp->next;
+  }
+  printf("NULL");
+  return 0;
+}
